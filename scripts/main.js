59a8795e1e0520abf38c6de00bf7cd9a4d9262cc
michael Greene <greeneink4@gmail.com>
2017-09-21 19:39:30 -0600

    Commit

--- scripts/main.js
+++ scripts/main.js
@@ -87,7 +87,7 @@
     }
 
     var myButton = document.querySelector('button');
-    var myHeading = document.querySelector('h3');
+    var myHeading = document.querySelector('h1');
 
     function setUserName() {
       var myName = prompt('Please enter your name');
